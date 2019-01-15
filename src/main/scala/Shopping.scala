aee67ebf97b44392ff01db1f82a44221382fa8f6
Bilal Khan <dijanmatheson@gmail.com>
2019-01-15 11:53:23 +0000

    Task 2

--- src/main/scala/Shopping.scala
+++ src/main/scala/Shopping.scala
@@ -1,6 +1,6 @@
 object Shopping extends App {
 
-  val shopping = List("Apples", "Oranges", "Apples")
+  val shopping = List("Apples", "Oranges", "Apples", "Oranges", "Oranges")
   var total = 0.00
   var countApples = 0
   var countOranges = 0
@@ -16,15 +16,15 @@ object Shopping extends App {
     }
   }
 
-  if (countApples==1) total = total+ 0.60
+  if (countApples==1||countApples==2) {total = total+ 0.60}
 
-  if((countApples>1) && (countApples%2 != 0)) {
+  if(countApples>1 && countApples%2 != 0) {
         total = total + 0.60
-      }
+  }
 
-    if(countOranges%3==0){
+  if(countOranges%3==0){
       total = total +((countOranges/3)*2*0.25)
-    }
+  }
   if(countOranges%3!=0){
       total = total +(countOranges*0.25)
     }
