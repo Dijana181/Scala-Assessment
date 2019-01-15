477da884eae4bd7bc0643e61e240edbd37a20c3e
Bilal Khan <dijanmatheson@gmail.com>
2019-01-15 11:15:44 +0000

    Git init

---
+++ src/main/scala/Shopping.scala
@@ -0,0 +1,25 @@
+object Shopping extends App {
+
+  val shopping = List("Apples", "Oranges", "Apples")
+  var total = 0.00
+  var countApples = 0
+  var countOranges = 0
+
+  for(i <- 0 until shopping.length){
+
+    if (shopping(i)== "Apples"){
+      countApples + 1
+
+      if(countApples %2 != 0) {
+        total = total + 0.60
+      }
+    }
+
+    if(shopping(i)=="Oranges"){
+      total = total+0.25
+    }
+
+  }
+println(s"£ $total")
+
+}
