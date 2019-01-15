5648e293298f22e45edb7eae0f8a8bf9c01a172e
Bilal Khan <dijanmatheson@gmail.com>
2019-01-15 11:44:55 +0000

    Shopping project

--- src/main/scala/Shopping.scala
+++ src/main/scala/Shopping.scala
@@ -5,22 +5,30 @@ object Shopping extends App {
   var countApples = 0
   var countOranges = 0
 
-  for(i <- 0 until shopping.length){
+  for(i <- 0 until shopping.length) {
 
-    if (shopping(i)== "Apples"){
-      countApples + 1
-
-      if(countApples %2 != 0) {
-        total = total + 0.60
-      }
+    if (shopping(i) == "Apples") {
+      countApples += 1
     }
 
     if(shopping(i)=="Oranges"){
-      total = total+0.25
+      countOranges += 1
+    }
+  }
+
+  if (countApples==1) total = total+ 0.60
+
+  if((countApples>1) && (countApples%2 != 0)) {
+        total = total + 0.60
+      }
+
+    if(countOranges%3==0){
+      total = total +((countOranges/3)*2*0.25)
+    }
+  if(countOranges%3!=0){
+      total = total +(countOranges*0.25)
     }
 
-  }
 println(s"£ $total")
-
 
 }
